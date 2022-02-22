be9e0997792f438c1fa940f6f05ee3783ac6365c
전병훈 <83635402+Carolsuki@users.noreply.github.com>
2022-02-22 11:30:24 +0900

    Update assult_judgment.py

--- project/20220130/refactoring/assult_judgment.py
+++ project/20220130/refactoring/assult_judgment.py
@@ -1,15 +1,12 @@
-from assult_print import str_print_func
+import assult_print as a_print
+import assult_active as a_active
 
-str_page_main ='''
-● 1. 게임 시작
-● 2. 세이브 파일 불러오기
-● 0. 게임 종료
-'''
+list_page = ["main", "create_name", "load_save", "exit"]
 
 # 페이지 변수
-list_main_page = ["create_name", "load_save", "exit"]
+list_main_page = ["create_name", "load_save", "exit"] # 1. 게임 시작[0] 2. 세이브 파일 불러오기[1] 0. 게임 종료[2]
 
-# 정상 입력 판단 한수
+# 정상 입력 판단 함수
 def input_succeed_judgment_func(cmd_input):
     # 입력이 숫자인가?
     if(cmd_input.isdigit() == True):
@@ -35,11 +32,19 @@ def input_judgment_func(page, cmd_input):
 
 # 메인 화면 판단 함수
 def input_main_judgment_func(cmd_input):
-    if cmd_input == 1:
-        page = list_main_page(cmd_input + 1)
+    if cmd_input > -1 and cmd_input < 4:
+        page = list_main_page[cmd_input - 1]
+        page = a_active.active_func(page)
         return page
+    
+    else:
+        a_print.str_print_func("error")
+        return "main"
 
 # 출력 판단 함수
 def print_judgment_func(page):
-    if page == "main":
-        str_print_func(str_page_main)
+    if page in list_page:
+        a_print.str_print_func(page)
+    
+    else:
+        a_print.str_print_func("error")
