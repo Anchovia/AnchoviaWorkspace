9613cdabf97f0a301389bd3bd772d422f1bac146
전병훈 <83635402+Carolsuki@users.noreply.github.com>
2022-02-23 15:44:45 +0900

    Update assult_print.py

--- project/20220130/refactoring/assult_print.py
+++ project/20220130/refactoring/assult_print.py
@@ -1,3 +1,6 @@
+# 시스템 관련 변수
+string_logic = 0
+
 # 시스템 프린트 변수
 str_line = "=" * 120
 
@@ -16,25 +19,32 @@ str_page_main ='''
 ● 0. 게임 종료
 '''
 
-str_page_create_name = '''
+str_page_create_character = '''
 앞으로 여정을 함께할 에이전트의 이름을 입력해주세요(1~12자).
 '''
 
-str_page_load_save = "아직 구현되지 않은 콘텐츠입니다."
+str_page_load_save = '''
+아직 구현되지 않은 콘텐츠입니다.
+'''
 
-str_page_exit = "프로그램을 종료합니다."
+str_page_exit = '''
+프로그램을 종료합니다.
+'''
+
+str_page_prologue = '''
+프롤로그를 보시겠습니까?
+'''
 
 # 프린트 모음 딕셔너리
-dict_print = {"line" : str_line, "error" : str_error, "exit" : str_exit, "main" : str_page_main, "create_name" : str_page_create_name, "load_save" : str_page_load_save, "exit" : "아직 구현되지 않은 콘텐츠입니다."}
+dict_print = {"line" : str_line, "error" : str_error, "main" : str_page_main, "create_character" : str_page_create_character, "load_save" : str_page_load_save, "exit" : str_exit, "prologue" : str_page_prologue}
 
-class Class_Print:
-    def __init__(self, dict_print, page):
-        self.dict_print = dict_print
-        self.page = page
+def make_string_func(input):
+    if input in list(dict_print):
+        return dict_print[input]
+    else:
+        input = str(input)
+        return "\n" + input + "\n"
 
-    def __str__(self):
-        return self.dict_print["line"] + "\n" + self.dict_print[self.page] + "\n" + self.dict_print["line"] # 출력
-
-def str_print_func(page):
-    pt = Class_Print(dict_print, page)
-    print(pt)
+def str_print_func(data):
+    string = make_string_func(data)
+    print(dict_print["line"] + "\n" + string + "\n" + dict_print["line"])
