965daac7695e9167869561c4f5f4061004510036
전병훈 <83635402+Carolsuki@users.noreply.github.com>
2022-02-23 15:43:22 +0900

    Update assault_main.py

--- project/20220130/refactoring/assault_main.py
+++ project/20220130/refactoring/assault_main.py
@@ -4,12 +4,14 @@ import assult_page as a_page
 
 # 메인 함수
 def main():
-    class_page = a_page.Page()
+    class_page = a_page.Page() # 페이지 객체 생성
 
     while True:
-        a_judgment.print_judgment_func(class_page)
-        now_page = a_input.input_func(class_page)
-        class_page.page_transform(now_page)
+        print(class_page.now_page_output()) # 테스트용 현재 장면 프린트
+
+        a_judgment.print_judgment_func(class_page) # 페이지 객체를 출력 판단 함수 인자로 전달
+        now_page = a_input.input_func(class_page) # 입력 함수 실행 및 현재 페이지를 받아옴
+        class_page.page_update(now_page) # 현재 페이지 업데이트
 
 # 프로그램 실행
 if __name__== "__main__":
