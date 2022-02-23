c67b0a948ae83d3bab03cc845e912b4bcb21c1eb
전병훈 <83635402+Carolsuki@users.noreply.github.com>
2022-02-23 15:43:43 +0900

    Update assult_judgment.py

--- project/20220130/refactoring/assult_judgment.py
+++ project/20220130/refactoring/assult_judgment.py
@@ -1,10 +1,19 @@
 import assult_print as a_print
 import assult_active as a_active
 
-list_page = ["main", "create_name", "load_save", "exit"]
+# 출력 판단 함수
+def print_judgment_func(class_page):
 
-# 페이지 변수
-list_main_page = ["create_name", "load_save", "exit"] # 1. 게임 시작[0] 2. 세이브 파일 불러오기[1] 0. 게임 종료[2]
+    page_now = class_page.now_page_output() # 현재 페이지
+    list_page_all = class_page.list_page_all_output() # 전체 페이지 리스트
+
+    page_error = class_page.page_error_output() # 에러 페이지
+
+    if page_now in list_page_all:
+        a_print.str_print_func(page_now)
+    
+    else:
+        a_print.str_print_func(page_error)
 
 # 정상 입력 판단 함수
 def input_succeed_judgment_func(cmd_input):
@@ -21,30 +30,53 @@ def input_succeed_judgment_func(cmd_input):
         return False # 거짓 리턴
 
 # 입력 판단 함수
-def input_judgment_func(page, cmd_input):
-    cmd_input = int(cmd_input)
-    if page == "main":
-        page = input_main_judgment_func(cmd_input)
-        return page
-        
-    else:
-        return page
+def input_judgment_func(class_page, cmd_input):
+    page_now = class_page.now_page_output() # 현재 페이지 변수
 
-# 메인 화면 판단 함수
-def input_main_judgment_func(cmd_input):
+    page_main = class_page.page_main_output() # 메인 페이지 변수
+    page_create_character = class_page.page_create_character_output # 캐릭터 생성 페이지 변수
+
+    # 현재 페이지가 캐릭터 생성 페이지일 때
+    if page_now == page_create_character:
+        page_now = input_create_character_judgment_func(class_page, cmd_input)
+        return page_now
+
+    # 현재 페이지가 메인일 때
+    elif page_now == page_main:
+        page_now = input_main_judgment_func(class_page, cmd_input)
+        return page_now
+
+    else:
+        return page_now
+
+# 메인 화면창 판단 함수
+def input_main_judgment_func(class_page, cmd_input):
+    page_now = class_page.now_page_output() # 현재 페이지 변수
+
+    list_page_main = class_page.list_page_main_output() # 메인 페이지 리스트
+    
+    page_error = class_page.page_error_output() # 에러 페이지 변수
+
     if cmd_input > -1 and cmd_input < 4:
-        page = list_main_page[cmd_input - 1]
-        page = a_active.active_func(page)
-        return page
-    
-    else:
-        a_print.str_print_func("error")
-        return "main"
+        page_now = a_active.active_func(class_page, cmd_input)
+        page_now = list_page_main[cmd_input - 1]
 
-# 출력 판단 함수
-def print_judgment_func(page):
-    if page in list_page:
-        a_print.str_print_func(page)
+        return page_now
     
     else:
-        a_print.str_print_func("error")
+        a_print.str_print_func(page_error)
+        return page_now
+
+# 닉네임 생성창 판단 함수
+def input_create_character_judgment_func(class_page, cmd_input):
+    page_now = class_page.now_page_output() # 현재 페이지 변수
+    
+    page_error = class_page.page_error_output() # 에러 페이지 변수
+
+    if len(cmd_input) > 0 and len(cmd_input) < 13:
+        page_now = a_active.active_func(class_page, cmd_input)
+        return page_now
+    
+    else:
+        a_print.str_print_func(page_error)
+        return page_now
