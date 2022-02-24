c6e7b4de1048e87288bda17d6f17571e2eb87889
전병훈 <83635402+Carolsuki@users.noreply.github.com>
2022-02-24 23:16:19 +0900

    Update assault_player_data.py

--- project/20220130/refactoring/assault_player_data.py
+++ project/20220130/refactoring/assault_player_data.py
@@ -1,16 +1,8 @@
-class Player:
-    def __init__(self, player_name = None):
-        self.player_name = player_name
-    
-    def output_player_name(self):
-        return self.player_name
-    
-    def input_player_data(self, player_name):
+player_name = None
+
+class Player_Data:
+    def __init__(self, player_name = player_name):
         self.player_name = player_name
 
-def creat_name_func(cmd_input):
-    player = Player()
-    player.input_player_data(cmd_input)
-
-    player_name = player.output_player_name()
-    return player_name
+    def update_player_name(self, player_name):
+        self.player_name = player_name
