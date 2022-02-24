710b66c89a752468a9842924c4a548ef4a16d934
전병훈 <83635402+Carolsuki@users.noreply.github.com>
2022-02-24 23:14:28 +0900

    Update assault_input.py

--- project/20220130/refactoring/assault_input.py
+++ project/20220130/refactoring/assault_input.py
@@ -1,28 +1,8 @@
-import assault_print as a_print
-import assault_judgment as a_judgment
+# 시스템 문자열
+str_system_line = "=" * 120
 
-# 입력 함수
-def input_func(class_page):
-    cmd_input = input("입력: ") # 입력받음
-
-    page_now = class_page.now_page_output() # 현재 페이지 변수
-    page_create_character = class_page.page_create_character_output # 캐릭터 생성 페이지 변수
-
-    page_error = class_page.page_error_output() # 에러 페이지 변수
-
-    # 닉네임 생성창 한정
-    if page_now == page_create_character:
-        page_now = a_judgment.input_judgment_func(class_page, cmd_input) # 페이지 판단 함수로 페이지 객체와 입력받은 값 전달 및 현재 페이지 반환
-        class_page.page_update(page_now) # 현재 페이지 업데이트
-        return class_page # 페이지 객체 반환
-
-    # 입력이 숫자일 때
-    if a_judgment.input_succeed_judgment_func(cmd_input) == True:
-        cmd_input = int(cmd_input) # 입력을 숫자로 변환
-        page_now = a_judgment.input_judgment_func(class_page, cmd_input) # 페이지 판단 함수로 페이지 객체와 입력받은 값 전달 및 현재 페이지 반환
-        return page_now # 페이지 객체 반환
-
-    # 입력이 숫자가 아닐때
-    else:
-        a_print.str_print_func(page_error) # 오류문 출력
-        return page_now # 페이지 객체 반환
+def input_func():
+    print(str_system_line)
+    cmd_input = input("입력: ")
+    print(str_system_line)
+    return cmd_input
